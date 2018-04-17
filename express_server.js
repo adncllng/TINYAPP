7394a9e3fcecdbeabac4778eee3448ecb74eee41
Aden Collinge <aden.collinge@gmail.com>
2018-04-17 18:19:53 +0000

    added keygenerator

--- express_server.js
+++ express_server.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 8080; // default port 8080
 app.set("view engine", "ejs");
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
@@ -11,6 +13,11 @@ let urlDatabase = {
 app.get("/", (req, res) => {
   res.end("Hello!");
 });
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   // console.log(templateVars)
@@ -29,6 +36,21 @@ app.get("/urls/:id", (req, res) => {
   res.render("urls_show", templateVars);
 });
 
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // debug statement to see POST parameters
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+function generateRandomString() {
+let randoString = "";
+while(randoString.length < 6){
+  let values = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
+  randoString += values[Math.round(Math.random()*(values.length-1))];
+}
+return randoString;
+}
+
