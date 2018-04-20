d6ae24b3d1e5f55d76b5961124cd09950e04e1fd
Aden Collinge <aden.collinge@gmail.com>
2018-04-20 21:18:41 +0000

    working on errors

--- express_server.js
+++ express_server.js
@@ -16,6 +16,12 @@ app.use(cookieSession({
 
 app.use(bodyParser.urlencoded({extended: true}));
 
+const error = {
+  400: "Bad Request ",
+  401: "Unauthorized ",
+  403: "Forbidden ",
+}
+
 let urlDatabase = {
   "b2xVn2": {
     url: "http://www.lighthouselabs.ca",
@@ -75,11 +81,14 @@ app.get("/", (req, res) => {
   if(req.session.user_id){
   res.redirect("/urls");
   } else {
-    res.redirect("/login")
+    res.redirect("/login");
   }
 });
 
 app.get("/login", (req, res) => {
+  if(req.session.user_id){
+    res.redirect("/urls")
+  }
   let templateVars = {
     user: users[req.session.user_id],
     urls: urlsForUserId(req.session.user_id)
@@ -88,10 +97,14 @@ app.get("/login", (req, res) => {
 });
 
 app.get("/register", (req, res) => {
+  if (req.session.user_id){
+    res.redirect("/urls");
+  } else {
   let templateVars = {
     user: users[req.session.user_id]
   }
   res.render("register", templateVars)
+}
 });
 
 app.get("/u/:shortURL", (req, res) => {
@@ -99,7 +112,7 @@ app.get("/u/:shortURL", (req, res) => {
     let longURL = urlDatabase[req.params.shortURL].url;
     res.redirect(longURL);
   } else {
-    res.send("Error: 400 Bad Request");
+    res.send("<h1>Error: 400</h1> <p>url does not exist</p> <a href='/'>back</a>");
   }
 });
 
@@ -110,38 +123,50 @@ app.get("/urls/new", (req, res) => {
   if(templateVars.user){
     res.render("urls_new", templateVars);
   }else{
-    res.render("login")
+    res.redirect("/login")
   }
 });
 
 app.get("/urls", (req, res) => {
+  if(req.session.user_id){
   let templateVars = {
     user: users[req.session.user_id],
     urls: urlsForUserId(req.session.user_id)
   };
   res.render("urls_index", templateVars);
+} else {
+    res.send("Error:")
+
+}
 });
 
 app.get("/urls/:id", (req, res) => {
   let userUrls = urlsForUserId(req.session.user_id);
-  if(userUrls[req.params.id]){
+
+  if(urlDatabase[req.params.id]){
     let templateVars = {
       user: users[req.session.user_id],
       shortURL: req.params.id,
-      urls: userUrls
+      urls: userUrls,
+      error: ""
     };
-    res.render("urls_show", templateVars);
-}else {
-  res.send("Error 403:  forbidden")
-}
+    if(userUrls[req.params.id]){
+      res.render("urls_show", templateVars);
+    } else {
+        templateVars = {...templateVars, error: error["403"]}
+      res.render("urls_show", templateVars);
+      }
+  } else {
+    res.redirect("/urls");
+  }
 });
 
-app.get("/logout", (req, res) => {
+app.post("/logout", (req, res) => {
   req.session = null;
   res.redirect('/urls');
 })
 
-app.post("/urls/new", (req, res) => {
+app.post("/urls", (req, res) => {
   var shortURL = generateRandomString();
   urlDatabase[shortURL] = {
     url: req.body.longURL,
@@ -154,15 +179,17 @@ app.post("/urls/:id/delete",(req, res) => {
  if(urlsForUserId(req.session.user_id)[req.params.id]){
    delete (urlDatabase[req.params.id]);
   res.redirect('/urls');
-  }else{}
+  }else{
+    res.send(error["403"])
+  }
 })
 
-app.post("/urls/:id/update",(req, res) => {
+app.post("/urls/:id",(req, res) => {
   if(urlsForUserId(req.session.user_id).hasOwnProperty(req.params.id)){
     urlDatabase[req.params.id].url = req.body.newLongURL;
     res.redirect('/urls');
 }else {
-  res.redirect('/urls');
+  res.send(error["403"]);
 }
 })
 
@@ -172,7 +199,7 @@ app.post("/login", (req, res) => {
     req.session.user_id = userId;
     res.redirect('/urls');
   } else {
-    res.send("Error 403 wrong password or email");
+    res.send("<h1>Error: 403</h1> <p>incorrect email or password</p> <a href='/login'>try again</a>");
   }
 })
 
@@ -180,7 +207,7 @@ app.post("/register", (req, res) => {
   if(!req.body.email || !req.body.password ){
     res.send('Error 400: empty forms! ')
   }else if (getId(users, "email", req.body.email)){
-    res.send('Error 404: email already exists!');
+    res.send("<h1>Error: 404</h1> <p>user already exists!</p> <a href='/register'>try again</a>");
   }else {
     let user_id = generateRandomString();
     let password = req.body.password;
